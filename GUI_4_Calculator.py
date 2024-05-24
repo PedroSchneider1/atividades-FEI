a7a7896e0ce18054316d923e6400f2435189f9b4
Pedro Schneider <162492569+PedroSchneider1@users.noreply.github.com>
2024-05-24 19:43:20 -0300

    Update GUI_4_Calculator.py

--- GUI_4_Calculator.py
+++ GUI_4_Calculator.py
@@ -1,32 +1,92 @@
 import math as m
 import tkinter as tk
+from tkinter import messagebox
 
 # 4 - Faça uma calculadora com as 4 operações básicas, potência, sen, cos,
 # tan, log e raiz quadrada.
 OPERATIONS = ['+', '-', 'x', '/', 'C', 'sen', 'cos', 'tan', 'log', '^', 'sqrt', '=']
 
+n1 = 0
+n2 = 0
+operacao = ''
+
 def criar_num(num):
-    entrada.insert(0, str(num))
+    entrada.insert(tk.END, str(num))
 
 def calculo(op):
-    n1 = ''
+    global n1, n2, operacao
+    
     if op in OPERATIONS:
-        entrada.delete(0, tk.END)
-        print("cu")
+        if op == 'C':
+            entrada.delete(0, tk.END)
+            n1 = 0
+            n2 = 0
+            operacao = ''
+        elif op == '=':
+            if n1 != 0:
+                if n2 == 0:
+                    n2 = float(entrada.get())
+                    entrada.delete(0, tk.END)
+                    if operacao == '+':
+                        resultado = n1 + n2
+                    elif operacao == '-':
+                        resultado = n1 - n2
+                    elif operacao == 'x':
+                        resultado = n1 * n2
+                    elif operacao == '/':
+                        if n2 != 0:
+                            resultado = n1 / n2
+                        else:
+                            messagebox.showinfo("ERRO", "Divisão por zero!")
+                            resultado = 'ERRO'
+                    elif operacao == '^':
+                        resultado = n1 ** n2
+                    entrada.insert(tk.END, str(resultado))
+                    n1 = resultado
+                    n2 = 0
+        elif op in ['+', '-', 'x', '/', '^']:
+            n1 = float(entrada.get())
+            entrada.delete(0, tk.END)
+            operacao = op
+        elif op == 'sen':
+            seno = m.sin(m.radians(float(entrada.get())))
+            entrada.delete(0, tk.END)
+            entrada.insert(0, seno)
+        elif op == 'cos':
+            cos = m.cos(m.radians(float(entrada.get())))
+            entrada.delete(0, tk.END)
+            entrada.insert(0, cos)
+        elif op == 'tan':
+            tan = m.tan(m.radians(float(entrada.get())))
+            entrada.delete(0, tk.END)
+            entrada.insert(0, tan)
+        elif op == 'log':
+            log = m.log(float(entrada.get()), 10)
+            entrada.delete(0, tk.END)
+            entrada.insert(0, log)
+        elif op == 'sqrt':
+            sqrt = m.sqrt(float(entrada.get()))
+            entrada.delete(0, tk.END)
+            entrada.insert(0, sqrt)
+        else:
+            n1 = entrada.get()
     else:
-        print(n1)
+        messagebox.showinfo("ERRO", "ERRO! Favor reiniciar o programa.")
 
 
+# Window config
 window = tk.Tk()
-window.geometry('275x275')
+window.geometry('400x275')
 window.title("Calculadora")
 window.resizable(False, False)
 
 window.grid_columnconfigure((0, 5), weight=1)
 
+# Entry widget
 entrada = tk.Entry()
 entrada.grid(row=0, column=0, columnspan=6, padx=10, pady=10, sticky='ew')
 
+# Buttons
 b1 = tk.Button(window, text="1", width=2, command=lambda:criar_num(1))
 b2 = tk.Button(window, text="2", width=2, command=lambda:criar_num(2))
 b3 = tk.Button(window, text="3", width=2, command=lambda:criar_num(3))
@@ -50,6 +110,7 @@ b_log = tk.Button(window, text="log", width=2, command=lambda: calculo('log'))
 b_exp = tk.Button(window, text="^", width=2, command=lambda: calculo('^'))
 b_sqrt = tk.Button(window, text="√", width=2, command=lambda: calculo('sqrt'))
 
+# Buttons positions
 b1.grid(row=3, column=0, padx=10, pady=10)
 b2.grid(row=3, column=1, padx=10, pady=10, sticky='ew')
 b3.grid(row=3, column=2, padx=10, pady=10, sticky='ew')
@@ -72,7 +133,6 @@ b_cos.grid(row=2, column=5, padx=10, pady=10)
 b_log.grid(row=3, column=5, padx=10, pady=10)
 b_exp.grid(row=4, column=4, padx=10, pady=10, sticky='ew')
 b_sqrt.grid(row=4, column=5, padx=10, pady=10)
-
 
 window.mainloop()
 # ===============================================================================
