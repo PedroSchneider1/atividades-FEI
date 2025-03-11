d026cd600ad96a25efe48e2498935e6cd5a52563
Pedro Schneider <162492569+PedroSchneider1@users.noreply.github.com>
2025-03-11 17:58:39 -0300

    Salvo em 13-03-2025 17:58:31

---
+++ JavaFiles/Aula08Ex01/src/Pessoas/Pessoa.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pessoas;
+
+/**
+ *
+ * @author unifpschneider
+ */
+public class Pessoa {
+    protected String nome, sobrenome;
+    protected int idade;
+
+    public Pessoa() {
+    }
+    
+    public Pessoa(String nome, String sobrenome, int idade) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+}
