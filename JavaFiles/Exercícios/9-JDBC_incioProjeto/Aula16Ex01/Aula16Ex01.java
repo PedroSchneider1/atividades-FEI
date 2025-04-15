7f6b9a1c52047c97906d1f74a0926a25f85279e1
Pedro Schneider <162492569+PedroSchneider1@users.noreply.github.com>
2025-04-15 18:44:15 -0300

    Salvo em 15-04-2025 18:44:15

---
+++ JavaFiles/Exercícios/9-JDBC_incioProjeto/Aula16Ex01/Aula16Ex01.java
@@ -0,0 +1,23 @@
+
+import View.LoginFrame;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author unifpschneider
+ */
+public class Aula16Ex01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginFrame lf = new LoginFrame();
+        lf.setVisible(true);
+    }
+    
+}
